685589cb3512b0588e3df65573d212b160d55694
Berke Dalar <berkedalar4@gmail.com>
2023-06-26 06:52:12 +0300

    Initial commit

---
+++ add_new.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<title>PHP Crud Application</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-light justify-content-center fs-3 mb-5"
+        style="background-color: darkblue;">
+            PHP Crd Application
+        </nav>
+    <div class="container">
+        <div class="text-center mb-4">
+        <h3>Add New User</h3>
+        <p class="text-muted">Complete the form below to add new user</p>
+
+        </div>
+        <div class="container d-flex justify-content-center">
+            <form action="" merhod="post" style="width:50vw; min-width:300px">
+                <div class="row">
+                    <div class="col">
+                        <label class="form-label">First Name:</label></label>
+                        <input type="text" class="form-control" name="fist-name"
+                        placeholder="Albert">
+                    </div>
+                    <div class="col">
+                        <label class="form-label">Last Name:</label></label>
+                        <input type="text" class="form-control" name="last-name"
+                        placeholder="Einstein">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </body>
+</html>
