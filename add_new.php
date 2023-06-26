19ad228ccf8ab489b2ba04d54b97006191115032
Berke Dalar <berkedalar4@gmail.com>
2023-06-26 17:33:56 +0300

    updateee

--- add_new.php
+++ add_new.php
@@ -1,3 +1,26 @@
+<?php
+include "db_conn.php";
+if(isset($_POST['submit'])){
+    $first_name = $_POST['$first_name'];
+    $last_name = $_POST['$last_name'];
+    $email = $_POST['$email'];
+    $gender = $_POST['$gender'];
+
+    $sql = "INSERT INTO `crud`(`id`, `first_name`, `last_name`, `email`, `gender`)
+     VALUES (NULL,'$first_name','$last_name','$email','$gender')";
+
+     $result = mysqli_query($conn, $sql);
+
+     if($result){
+        header("Location: index.php?msg=New record created successfully");
+     }else {
+        echo "Failed. " .mysqli_error($conn);
+     }
+}
+
+?>
+
+
 <!DOCTYPE html>
 
 <html>
@@ -14,7 +37,7 @@
     </head>
     <body>
         <nav class="navbar navbar-light justify-content-center fs-3 mb-5"
-        style="background-color: darkblue;">
+        style="background-color: seagreen;">
             PHP Crd Application
         </nav>
     <div class="container">
@@ -25,7 +48,7 @@
         </div>
         <div class="container d-flex justify-content-center">
             <form action="" merhod="post" style="width:50vw; min-width:300px">
-                <div class="row">
+                <div class="row mb-3">
                     <div class="col">
                         <label class="form-label">First Name:</label></label>
                         <input type="text" class="form-control" name="fist-name"
@@ -37,6 +60,25 @@
                         placeholder="Einstein">
                     </div>
                 </div>
+                <div class="mb-3">
+                
+                    <label class="form-label">Email:</label></label>
+                    <input type="text" class="form-control" name="fist-name"
+                    placeholder="name@example.com">
+                    
+                </div>
+                <div class="form-group mb-3">
+                    <label>Gender:</label> &nbsp;
+                    <input type="radio" class="form-check-input" name="gender" id="male"value="male">
+                    <label for="male" class="form-input-label">Male</label>
+                    <input type="radio" class="form-check-input" name="gender" id="female"value="female">
+                    <label for="female" class="form-input-label">Female</label>
+                </div>
+                    <div>
+                        <button type="submit" class="btn btn-success" name="submit">Save</button>
+                        <a href="index.php" class="btn btn-danger">Cancel</a>
+                    </div>
+
             </form>
         </div>
     </div>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<title>PHP Crud Application</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-light justify-content-center fs-3 mb-5"
+        style="background-color: seagreen;">
+            PHP Crud Application
+        </nav>
+    <div class="container">
+        <a href="add_new.php" class="btn btn-dark mb-3">Add New</a>
+        <table class="table table-hover text-center">
+  <thead class="table-dark">
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Gender</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+     <?php
+        include "db_conn.php";
+            $sql = "SELECT * FROM `crud`";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_assoc($result)){
+                ?>
+                <tr>
+                    <th><?php echo $row['id']?></th>
+                    <th><?php echo $row['first_name']?></th>
+                    <th><?php echo $row['last_name']?></th>
+                    <th><?php echo $row['email']?></th>
+                    <th><?php echo $row['genre']?></th>
+                    <td>
+                        <a href="" class="link-dark"><i class="fa-solid fa-pen-to-square
+                        fs-5 me-3"></i></a>
+                        <a href="" class="link-dark"><i class="fa solid fa-trash fs-5"></i></a>
+                    </td>
+                </tr>          
+            <?php
+            }
+          ?>
+   
+  </tbody>
+</table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </body>
+</html>
